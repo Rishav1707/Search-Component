11ae430dfacd0aeac98ee3d0693e045b9e0870a4
Rishav Raj <rishavraj1707music@gmail.com>
2023-04-21 23:01:09 +0530

    [feat] Add Dark Mode

--- Recipes/src/App.jsx
+++ Recipes/src/App.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 import { foods, filterItems } from "./Recipes";
+import Theme from "./Theme";
 import "./App.css";
 
 export default function FilterableList() {
   const [query, setQuery] = useState("");
+  const [theme, setTheme] = useState("light");
   const results = filterItems(foods, query);
 
   function handleChange(e) {
     setQuery(e.target.value);
   }
 
+  function toggleTheme() {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
   return (
-    <>
+    <div className={`App ${theme}`}>
+      <Theme toggleTheme={toggleTheme} theme={theme} />
       <h3 className="menu">Food Menu🍻</h3>
       <SearchBar query={query} onChange={handleChange} />
       <List items={results} />
       <h3 className="menu">Enjoy your meal❣️</h3>
-    </>
+    </div>
   );
 }
 
